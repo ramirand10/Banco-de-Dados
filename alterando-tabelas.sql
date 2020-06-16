e2ade54256ce448544a5cab882518933a409ca89
Raphael Miranda <raphaelmartins521@gmail.com>
2020-06-16 14:22:03 -0300

    alterando-tabelas.sql

---
+++ alterando-tabelas.sql
@@ -0,0 +1,59 @@
+/* ALTER TABLE */
+
+/* ALTERANDO O NOME DE UMA COLUNA - CHANGE */
+
+ALTER TABLE PRODUTO
+CHANGE PRECO VALOR_UNITARIO INT NOT NULL;
+
+DESC PRODUTO;
+
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO      | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO   | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNITARIO | int(11)     | NO   |     | NULL    |                |
+| FRETE          | float(10,2) | NO   |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+ALTER TABLE PRODUTO
+CHANGE VALOR_UNITARIO VALOR_UNITARIO INT;
+
+DESC PRODUTO;
+
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO      | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO   | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNITARIO | int(11)     | YES  |     | NULL    |                |
+| FRETE          | float(10,2) | NO   |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+/* MODIFY - ALTERANDO O TIPO */
+
+ALTER TABLE PRODUTO
+MODIFY VALOR_UNITARIO VARCHAR(50) NOT NULL;
+
+/* ADICIONANDO COLUNAS */
+
+ALTER TABLE PRODUTO
+ADD PESO FLOAT(10,2) NOT NULL;
+
+/* APAGANDO UMA COLUNA */
+
+ALTER TABLE PRODUTO
+DROP COLUMN PESO;
+
+/* ADICIONANDO UMA COLUNA NA ORDEM ESPECIFICA */
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+AFTER NOME_PRODUTO;
+
+ALTER TABLE PRODUTO
+DROP COLUMN PESO;
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+FIRST;
